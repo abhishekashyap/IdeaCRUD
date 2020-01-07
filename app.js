8db58b3d4dbf55dfaadc367acebe02a1354d3437
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-07 21:43:55 +0530

    Add ideas page ready and view set

--- app.js
+++ app.js
@@ -45,6 +45,11 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Add Idea form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add')
+})
+
 const port = 5000;
 
 app.listen(port, () => {

2961f78d2c7a0ca05ba18180da9ff94dffe8d66f
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-07 18:43:32 +0530

    Implemented Schema

---
+++ models/Idea.js
@@ -0,0 +1,23 @@
+/*
+    Although MongoDB is a no SQL database, i.e. it doesn't require Schemas, it is good idea to create Schemas
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const IdeaSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('ideas', IdeaSchema);

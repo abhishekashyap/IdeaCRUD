603d7b63f450489fef292493d764b2e04932eee1
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-12 22:19:12 +0530

    Cleaning up code: Created ideas route module

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express(); // Initializing the express app by express function
 
+// Load routes
+const ideas = require('./routes/ideas');
+
 /*
 Installing mongodb 
 
@@ -29,10 +32,6 @@ mongoose.connect('mongodb://localhost/ideas-dev', {
         console.log('MongoDB Connected')
     })
     .catch(err => console.log(err));
-
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
 
 // Handlebars middleware
 app.engine('handlebars', exphbs({
@@ -79,99 +78,8 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Ideas index route 
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({
-            date: 'desc'
-        })
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
-            });
-        })
-})
-
-// Get ideas for edit
-app.get('/ideas/edit/:id', (req, res) => {
-    // :id acts as a placeholder, for the id of the idea from the DB
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            })
-        })
-})
-
-// Add Idea form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-})
-
-// Process form
-app.post('/ideas', (req, res) => {
-    // Since in HTML form we are using post method
-    console.log(req.body);
-
-    // Form validation
-    let errors = [];
-    if (!req.body.title) {
-        errors.push({ text: 'Please add a title' })
-    }
-
-    if (!req.body.details) {
-        errors.push({ text: 'Please add some details' })
-    }
-
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Idea added')
-                res.redirect('/ideas')
-            })
-    }
-});
-
-// Edit idea and update DB
-app.put('/ideas/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            // Updated values
-            idea.title = req.body.title;
-            idea.details = req.body.details;
-
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', 'Idea updated');
-                    res.redirect('/ideas');
-                })
-        })
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-    Idea.deleteOne({_id: req.params.id})
-    .then(() => {
-        console.log('Deleted entry');
-        req.flash('success_msg', 'Idea removed');
-        res.redirect('/ideas');
-    });
-});
+// Ideas routes
+app.use('/ideas', ideas);
 
 const port = 5000;
 
---
+++ routes/ideas.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+// Load Idea Model
+require('../models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Ideas index route 
+router.get('/', (req, res) => {
+    Idea.find({})
+        .sort({
+            date: 'desc'
+        })
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            });
+        })
+})
+
+// Get ideas for edit
+router.get('/edit/:id', (req, res) => {
+    // :id acts as a placeholder, for the id of the idea from the DB
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea: idea
+            })
+        })
+})
+
+// Add Idea form
+router.get('/add', (req, res) => {
+    res.render('ideas/add');
+})
+
+// Process form
+router.post('', (req, res) => {
+    // Since in HTML form we are using post method
+    console.log(req.body);
+
+    // Form validation
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({ text: 'Please add a title' })
+    }
+
+    if (!req.body.details) {
+        errors.push({ text: 'Please add some details' })
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                req.flash('success_msg', 'Idea added')
+                res.redirect('/ideas')
+            })
+    }
+});
+
+// Edit idea and update DB
+router.put('/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            // Updated values
+            idea.title = req.body.title;
+            idea.details = req.body.details;
+
+            idea.save()
+                .then(idea => {
+                    req.flash('success_msg', 'Idea updated');
+                    res.redirect('/ideas');
+                })
+        })
+});
+
+// Delete Idea
+router.delete('/:id', (req, res) => {
+    Idea.deleteOne({ _id: req.params.id })
+        .then(() => {
+            console.log('Deleted entry');
+            req.flash('success_msg', 'Idea removed');
+            res.redirect('/ideas');
+        });
+});
+
+module.exports = router;

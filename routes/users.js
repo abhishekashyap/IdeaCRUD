9db94dea478af33eb3bc542e31c655b212aa4f6a
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-13 22:01:26 +0530

    Added encryption using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // For hashing passwords
+//const passport = require('passport');
 
 const router = express.Router();
+
+// User model
+require('../models/User');
+const User = mongoose.model('users');
 
 router.get('/login', (req, res) => {
     res.render('users/login');
@@ -29,7 +36,26 @@ router.post('/signup', (req, res) => {
             email: req.body.email,
         })
     } else {
-        res.send('Passed');
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                .then(user => {
+                    req.flash('success_msg', 'You are now registered and can login');
+                    res.redirect('/users/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    return;
+                })
+            })
+        })
     }
 
 });

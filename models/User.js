926c14bdc29ac4751ee5961e1cbb665f2a2cbbef
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-13 20:12:01 +0530

    Added signup validation

---
+++ models/User.js
@@ -0,0 +1,27 @@
+/*
+    Although MongoDB is a no SQL database, i.e. it doesn't require Schemas, it is good idea to create Schemas
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('users', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,35 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-    res.render('users/login')
+    res.render('users/login');
 });
 
 router.get('/signup', (req, res) => {
     res.render('users/signup');
+});
+
+router.post('/signup', (req, res) => {
+    let errors = [];
+
+    if (req.body.password != req.body.password2) {
+        errors.push({text: 'Passwords do not match'});
+    }
+
+    if(req.body.password.length < 8) {
+        errors.push({text: 'Password should be atleast 8 characters long'});
+    }
+
+    if (errors.length > 0) {
+        // Re-rendering the form with details
+        res.render('users/signup', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+        })
+    } else {
+        res.send('Passed');
+    }
+
 });
 
 module.exports = router;
